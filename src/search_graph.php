ae8327cb45a1a8819f32abf77a56f294635170f8
algins <igors.algins@gmail.com>
2022-03-09 10:59:04 +0200

    add dijkstras search

--- src/search_graph.php
+++ src/search_graph.php
@@ -25,3 +25,56 @@ function breadthFirst(array $graph, string $key, string $searchKey): ?array
 
     return null;
 }
+
+/**
+ * Find lowest cost between two nodes
+ */
+function dijkstras(array $graph, string $startNode, string $endNode): ?int
+{
+    if (!array_key_exists($startNode, $graph) || !array_key_exists($endNode, $graph)) {
+        return null;
+    }
+
+    $processed = [];
+    $costs = [];
+    $parents = [];
+
+    foreach (array_keys($graph) as $node) {
+        $costs[$node] = $graph[$startNode][$node] ?? PHP_INT_MAX;
+        $parents[$node] = array_key_exists($node, $graph[$startNode]) ? $startNode : null;
+    }
+
+    $findLowestCostNode = function (array $costs) use (&$processed) {
+        $lowestCost = PHP_INT_MAX;
+        $lowestCostNode = null;
+
+        foreach ($costs as $node => $cost) {
+            if ($cost < $lowestCost && !in_array($node, $processed)) {
+                $lowesCost = $cost;
+                $lowestCostNode = $node;
+            }
+        }
+
+        return $lowestCostNode;
+    };
+
+    $lowestCostNode = $findLowestCostNode($costs);
+
+    while ($lowestCostNode) {
+        $cost = $costs[$lowestCostNode];
+        $neighbours = $graph[$lowestCostNode];
+
+        foreach ($neighbours as $neighbourNode => $neighbourCost) {
+            $newCost = $cost + $neighbourCost;
+            if ($costs[$neighbourNode] > $newCost) {
+                $costs[$neighbourNode] = $newCost;
+                $parents[$neighbourNode] = $lowestCostNode;
+            }
+        }
+
+        $processed[] = $lowestCostNode;
+        $lowestCostNode = $findLowestCostNode($costs);
+    }
+
+    return $costs[$endNode];
+}
--- tests/SearchGraphTest.php
+++ tests/SearchGraphTest.php
@@ -5,6 +5,7 @@ namespace Algins\PHPAlgo\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function Algins\PHPAlgo\SearchGraph\breadthFirst;
+use function Algins\PHPAlgo\SearchGraph\dijkstras;
 
 class SearchGraphTest extends TestCase
 {
@@ -13,21 +14,32 @@ class SearchGraphTest extends TestCase
     public function setUp(): void
     {
         $this->graph = [
-            'a' => ['c', 'b', 'd'],
-            'b' => ['e', 'f'],
-            'c' => ['f'],
-            'd' => ['g'],
+            'a' => ['c' => 6, 'b' => 2, 'd' => 1],
+            'b' => ['e' => 3, 'f' => 5],
+            'c' => ['f' => 1],
+            'd' => ['g' => 6],
             'e' => [],
             'f' => [],
-            'g' => ['h'],
+            'g' => ['h' => 0],
             'h' => [],
         ];
     }
 
     public function testBreadthFirst(): void
     {
-        $this->assertEquals(['h'], breadthFirst($this->graph, 'a', 'g'));
+        $graph = array_map(fn($item) => array_keys($item), $this->graph);
+
+        $this->assertEquals(['h'], breadthFirst($graph, 'a', 'g'));
         $this->assertNull(breadthFirst([], 'a', 'g'));
-        $this->assertNull(breadthFirst($this->graph, 'a', 'i'));
+        $this->assertNull(breadthFirst($graph, 'a', 'i'));
+    }
+
+    public function testDijkstras(): void
+    {
+        $this->assertEquals(7, dijkstras($this->graph, 'a', 'f'));
+        $this->assertEquals(6, dijkstras($this->graph, 'd', 'h'));
+        $this->assertNull(dijkstras([], 'a', 'f'));
+        $this->assertNull(dijkstras($this->graph, 'x', 'f'));
+        $this->assertNull(dijkstras($this->graph, 'a', 'x'));
     }
 }
